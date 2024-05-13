3f62024c3bb761f18cd78b13b58b5339dc277d3b
ATISHAY JAIN <atishayj2202@gmail.com>
2024-05-14 00:34:38 +0530

    Update footer.tsx

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 export default async function Footer() {
     return(
-        <footer className="flex flex-row justify-evenly w-full items-center">
+        <footer className="flex flex-row justify-evenly w-full items-center bg-gray-900 text-white">
             <div className="flex flex-row menu_container justify-between text-xs">
                 <span>© Pranjul Mangla</span>
                 <Link href="/projects">
